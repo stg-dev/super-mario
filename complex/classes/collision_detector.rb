4033ccea65cec7b6e60745f06734d3395b0e8e88
Benjamin-Tec <meyer.benjamin15@gmail.com>
2020-04-20 15:31:35 +0200

    BenniCollisionDetector implemented

--- complex/classes/collision_detector.rb
+++ complex/classes/collision_detector.rb
@@ -28,8 +28,32 @@ class CollisionDetector
 
     end
   end
+end
 
-  def compare()
+class BenniCollisionDetector
+  def detect_collisions(elements)
+    i = 1
+    e = elements[0]
+    while i < elements.length
+      check_collision(e, elements[i]).collisions.each_key do |key, val|
+        if val && (e.collisions[key] == false)
+          e.collisions[key] = true
+        end
+      end
+      i += 1
+    end
+  end
 
+  def check_collision(e1, e2)
+    dtop = e1.y_pos - e2.y_pos + e2.height
+    dbottom = e1.y_pos + e1.height - e2.y_pos
+    dleft = e1.x_pos - e2.x_pos + e2.width
+    dright = e1.x_pos + e1.width - e2.x_pos
+    # jhk
+    e1.collisions["top"] = dtop < 0 && dtop <= -(e2.height)
+    e1.collisions["bottom"] = dbottom < 0 && dbottom <= -(e2.height)
+    e1.collisions["right"] = dright < 0 && dright <= -(e2.width)
+    e1.collisions["left"] = dleft < 0 && dleft <= -(e2.width)
+    e1
   end
 end
