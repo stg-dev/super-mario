d70cbc692aecb20ba53c6870c6bb2b7e32f1af95
Benjamin-Tec <meyer.benjamin15@gmail.com>
2020-04-21 14:11:42 +0200

    optimization of collision_detector

--- complex/classes/cloud.rb
+++ complex/classes/cloud.rb
@@ -4,7 +4,7 @@ require './complex/classes/parents/passive_object'
 class Cloud < PassiveObject
   def initialize(x_pos)
     super(x_pos, 20, -5, 0, Image.new(
-        'assets/cloud.bmp',
+        'assets/cloud.png',
         x: @x_pos, y: @y_pos,
         width: 80,
         height: 80,
--- complex/classes/collision_detector.rb
+++ complex/classes/collision_detector.rb
@@ -1,30 +1,25 @@
-# DEPRECATED: only implements window collisions
-class CollisionDetector
-  def detect_collisions
-    throw('Module deprecated!')
-  end
-end
-
 # Working! Includes improved Math and Logic
 class BenniCollisionDetector
-  def detect_collisions(entity, elements)
-    entity.collisions.each do |key, _|
-      entity.collisions[key] = false
-    end
-
-    overlay(entity.collisions, check_window(entity, 1066, 600))
-
-    elements.each do |element|
-      if element.is_a? CollisionObject
-        overlay(entity.collisions, { check_intersection(entity, element) => true })
+  def detect_collisions(elements)
+    elements.each do |entity|
+      next unless entity.is_a? LivingEntity
+      entity.collisions.each do |key, _|
+        entity.collisions[key] = false
       end
-    end
 
+      overlay(entity.collisions, check_window(entity, 1066, 600))
+
+      elements.each do |element|
+        if element.is_a? CollisionObject
+          overlay(entity.collisions, { check_intersection(entity, element) => true })
+        end
+      end
     # puts(entity.collisions)
+    end
   end
 
   def check_intersection(e1, e2)
-    if e1.x1 < e2.x2 && e1.x2 > e2.x1 && e1.y1 < e2.y2 && e1.y2 > e2.y1
+    if e1.x1 <= e2.x2 && e1.x2 >= e2.x1 && e1.y1 <= e2.y2 && e1.y2 >= e2.y1
       dx = (e2.x_pos + e2.width / 2) - (e1.x_pos + e1.width / 2)
       dy = (e2.y_pos + e2.height / 2) - (e1.y_pos + e1.height / 2)
 
